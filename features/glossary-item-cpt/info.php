a78cf20fb4cb83fc7e03420243c2c380db70cfa1
Adam Marash <adam@ediblesites.com>
2025-07-09 20:17:49 +0100

    Remove test CPT

--- features/glossary-item-cpt/info.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'name' => 'Glossary Item CPT',
-    'description' => 'Creates a custom post type for managing glossary items',
-    'default' => true,
-    'main_file' => 'glossary-item-cpt.php',
-]; 
