766e82e7ac6399afbaa8eda36a341fc843415f0e
Adam Marash <adam@ediblesites.com>
2025-07-09 20:05:13 +0100

    FAQ subset generator

---
+++ features/faq-shortcode/info.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'name' => 'FAQ Shortcode',
+    'description' => 'Shortcode [faq tags="..."] to generate FAQ subsets from template',
+    'default' => true,
+    'main_file' => 'faq-shortcode.php',
+]; 
