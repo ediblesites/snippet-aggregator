73d17c58be20e4bcb3f22499bda69043b1dc76dc
Adam Marash <adam@ediblesites.com>
2025-07-10 10:43:49 +0100

    Feature: add inline edit link on archive pages

---
+++ features/archive-quick-edit/info.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'name' => 'Archive Quick Edit',
+    'description' => 'Adds quick edit links to archive entries for users with edit permissions',
+    'default' => true,
+    'main_file' => 'archive-quick-edit.php',
+]; 
