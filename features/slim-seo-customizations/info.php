bc90d8bdaf17e383a8e19f96a976e5cedcdd3e4d
Adam Marash <adam@ediblesites.com>
2025-07-10 10:51:07 +0100

    Remove Slim SEO customizations

--- features/slim-seo-customizations/info.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'name' => 'Slim SEO Customizations',
-    'description' => 'Customizes Slim SEO behavior',
-    'default' => true,
-    'main_file' => 'slim-seo-customizations.php',
-]; 
