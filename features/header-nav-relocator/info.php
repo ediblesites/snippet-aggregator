c30ddf27df0fa674cc75b424e95e2be273308f3f
Adam Marash <adam@ediblesites.com>
2025-07-12 20:09:20 +0100

    Add feature Header Nav Relocator

---
+++ features/header-nav-relocator/info.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'name' => 'Header Nav Relocator',
+    'description' => 'Moves the logo and navigation section to just below the header element',
+    'default' => true,
+    'main_file' => 'header-nav-relocator.php',
+    'context' => 'frontend',
+]; 
