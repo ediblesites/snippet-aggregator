2fd1b8ecc70e084b43842396965f49c206a129d3
adam-marash <35106583+adam-marash@users.noreply.github.com>
2025-07-22 18:04:25 +0100

    Run Google Search only on front end, so it doesn't interfere with post lookups on back end.

--- features/google-search/info.php
+++ features/google-search/info.php
@@ -3,5 +3,6 @@ return [
     'name' => 'Google Search',
     'description' => 'Integrates Google search functionality into the site',
     'default' => true,
-    'main_file' => 'google-search.php'
+    'main_file' => 'google-search.php',
+    'context' => 'frontend',
 ]; 
