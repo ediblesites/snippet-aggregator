8101258f0596e03cd5b16d43ed9cfb4e025a56f9
Adam Marash <adam@ediblesites.com>
2025-07-23 07:37:35 +0100

    Add Github URL to plugin header

--- snippet-aggregator.php
+++ snippet-aggregator.php
@@ -2,8 +2,9 @@
 /**
  * Plugin Name: Snippet Aggregator
  * Description: A self-updating WordPress plugin that manages internal functionality through feature toggles.
- * Version: 1.0.76    
+ * Version: 1.0.78    
  * Author: Adam Marash
+ * Author URI: https://github.com/ediblesites/snippet-aggregator
  * GitHub Plugin URI: https://github.com/ediblesites/snippet-aggregator
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -15,7 +16,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('SNIPPET_AGGREGATOR_VERSION', '1.0.76');
+define('SNIPPET_AGGREGATOR_VERSION', '1.0.78');
 define('SNIPPET_AGGREGATOR_FILE', __FILE__);
 define('SNIPPET_AGGREGATOR_PATH', plugin_dir_path(__FILE__));
 define('SNIPPET_AGGREGATOR_URL', plugin_dir_url(__FILE__));
